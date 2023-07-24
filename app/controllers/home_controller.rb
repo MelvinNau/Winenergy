11f7144a0dcc393d81d8c3cd892ba7d61a2867f1
mello <melvin.naudion@gmail.com>
2023-07-24 09:29:46 +0200

    Fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,9 +9,9 @@ class HomeController < ApplicationController
             redirect_to admin_url(current_user.id) if current_user.admin?
         end 
         
-        @wines_better_scores = Wine.all_with_notes.sort_by { |wine| wine.score? }.reverse 
+        @wines_better_scores = Wine.all_with_notes.sort_by { |wine| wine.score? } 
 
-        wine_last_prices = Price.where(id: Price.select("MAX(id)").group(:wine_id)).pluck(:wine_id, :price).to_h
-        @wines_better_prices = Wine.all.sort_by { |wine| wine_last_prices[wine.id] || 0 }.take(4)
+        wine_first_prices = Price.where(id: Price.select("MIN(id)").group(:wine_id)).pluck(:wine_id, :price).to_h
+        @wines_better_prices = Wine.all.sort_by { |wine| wine_first_prices[wine.id] || 0 }.take(4)
     end
 end
